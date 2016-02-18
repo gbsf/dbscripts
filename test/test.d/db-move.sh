27c03499420d373e479e68ef1cfc54d92d247e7c
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2016-02-18 20:43:10 -0200

    Extract removed split package logic from db-move
    Also adds a new related test case
    
    Signed-off-by: Gabriel Souza Franco <gabrielfrancosouza@gmail.com>

--- test/test.d/db-move.sh
+++ test/test.d/db-move.sh
@@ -131,7 +131,7 @@ testMoveChangedSplitPackages() {
 
 	"${curdir}"/../../db-update
 
-	pushd "${TMP}/svn-packages-copy/pkg-split-a/trunk/" >/dev/null
+	pushd "${TMP}/svn-packages-copy/${pkgbase}/trunk/" >/dev/null
 	sed "s/pkgrel=1/pkgrel=2/g;s/pkgname=('pkg-split-a1' 'pkg-split-a2')/pkgname='pkg-split-a1'/g" -i PKGBUILD
 	arch_svn commit -q -m"remove pkg-split-a2; pkgrel=2" >/dev/null
 	popd >/dev/null
@@ -145,7 +145,7 @@ testMoveChangedSplitPackages() {
 
 	for arch in ${arches[@]}; do
 		checkPackage extra ${pkgbase}1-1-2-${arch}.pkg.tar.xz ${arch}
-		checkRemovedPackage extra ${pkgbase}2-1-2-${arch}.pkg.tar.xz ${arch}
+		checkRemovedPackage extra ${pkgbase}2 ${arch}
 	done
 }
 
@@ -154,34 +154,29 @@ testMoveChangedAnySplitPackages() {
 	local pkgbase='pkg-split-c'
 	local arch
 
-	archreleasePackage "${pkgbase}"
 	for arch in ${arches[@]}; do
-		cp "${pkgdir}"/${pkgbase}/${pkgbase}1-1-1-${arch}.pkg.tar.xz{,.sig} "${TMP}"
-		"${curdir}"/../../db-add extra ${arch} "${TMP}/${pkgbase}1-1-1-${arch}.pkg.tar.xz"
+		releasePackage extra ${pkgbase} ${arch} ${pkgbase}1
 	done
-	cp "${pkgdir}"/${pkgbase}/${pkgbase}2-1-1-any.pkg.tar.xz{,.sig} "${TMP}"
-	"${curdir}"/../../db-add extra all "${TMP}/${pkgbase}2-1-1-any.pkg.tar.xz"
+	releasePackage extra ${pkgbase} any ${pkgbase}2
 
 	"${curdir}"/../../db-update
 
-	pushd "${TMP}/svn-packages-copy/pkg-split-c/trunk/" >/dev/null
+	pushd "${TMP}/svn-packages-copy/${pkgbase}/trunk/" >/dev/null
 	sed "s/pkgrel=1/pkgrel=2/g;s/pkgname=('pkg-split-c1' 'pkg-split-c2')/pkgname='pkg-split-c1'/g" -i PKGBUILD
 	arch_svn commit -q -m"remove pkg-split-c2; pkgrel=2" >/dev/null
 	popd >/dev/null
 
-	archreleasePackage "${pkgbase}"
 	for arch in ${arches[@]}; do
-		cp "${pkgdir}"/${pkgbase}/${pkgbase}1-1-2-${arch}.pkg.tar.xz{,.sig} "${TMP}"
-		"${curdir}"/../../db-add testing ${arch} "${TMP}/${pkgbase}1-1-2-${arch}.pkg.tar.xz"
+		releasePackage testing ${pkgbase} ${arch} ${pkgbase}1
 	done
 
 	"${curdir}"/../../db-update
-	"${curdir}"/../../db-move testing extra all pkg-split-c1
+	"${curdir}"/../../db-move testing extra all ${pkgbase}1
 
 	for arch in ${arches[@]}; do
 		checkPackage extra ${pkgbase}1-1-2-${arch}.pkg.tar.xz ${arch}
 	done
-	checkRemovedAnyPackage extra ${pkgbase}2-1-1-${arch}.pkg.tar.xz
+	checkRemovedAnyPackage extra ${pkgbase}2
 }
 
 testMoveDebugPackages() {
@@ -204,6 +199,35 @@ testMoveDebugPackages() {
 		checkRemovedPackage testing ${pkgbase} ${arch}
 		checkRemovedPackage testing-${DEBUGSUFFIX} ${pkgbase}-${DEBUGSUFFIX} ${arch}
 	done
+}
+
+testMoveChangedDebugSplitPackages() {
+	local arches=('i686' 'x86_64')
+	local pkgbase='pkg-debug-b'
+	local arch
+
+	for arch in ${arches[@]}; do
+		releasePackage extra ${pkgbase} ${arch} ${pkgbase}1
+	done
+	releasePackage extra ${pkgbase} any ${pkgbase}2
+
+	"${curdir}"/../../db-update
+
+	pushd "${TMP}/svn-packages-copy/${pkgbase}/trunk/" >/dev/null
+	sed "s/pkgrel=1/pkgrel=2/g;s/pkgname=('pkg-debug-b1' 'pkg-debug-b2')/pkgname='pkg-debug-b2'/g" -i PKGBUILD
+	arch_svn commit -q -m"remove pkg-debug-b1; pkgrel=2" >/dev/null
+	popd >/dev/null
+
+	releasePackage testing ${pkgbase} any ${pkgbase}2
+
+	"${curdir}"/../../db-update
+	"${curdir}"/../../db-move testing extra all ${pkgbase}2
+
+	for arch in ${arches[@]}; do
+		checkRemovedPackage extra ${pkgbase}1 ${arch}
+		checkRemovedPackage extra-${DEBUGSUFFIX} ${pkgbase}1-${DEBUGSUFFIX} ${arch}
+	done
+	checkAnyPackage extra ${pkgbase}2-1-2-any.pkg.tar.xz
 }
 
 . "${curdir}/../lib/shunit2"
