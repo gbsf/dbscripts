76e5b4fa66eeba94da2da2300c97fefdd313b94f
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2016-02-18 23:14:14 -0200

    Handle debug packages in testing2x
    Also treat removed split packages
    
    Signed-off-by: Gabriel Souza Franco <gabrielfrancosouza@gmail.com>

--- test/test.d/testing2x.sh
+++ test/test.d/testing2x.sh
@@ -43,4 +43,52 @@ testTesting2xMultiArchPackage() {
 	checkRemovedAnyPackage testing pkg-any-a
 }
 
+
+testMoveChangedSplitDebugPackages() {
+	local arches=('i686' 'x86_64')
+	local pkgs=('pkg-debug-a' 'pkg-debug-b')
+	local arch
+
+	for arch in ${arches[@]}; do
+		releasePackage core pkg-debug-a ${arch}
+		releasePackage core pkg-debug-b ${arch} pkg-debug-b1
+	done
+	releasePackage extra pkg-debug-b any pkg-debug-b2
+
+	"${curdir}"/../../db-update
+
+	pushd "${TMP}/svn-packages-copy/pkg-debug-a/trunk/" >/dev/null
+	sed "s/pkgrel=1/pkgrel=2/g" -i PKGBUILD
+	arch_svn commit -q -m"bump pkgrel=2" >/dev/null
+	popd >/dev/null
+
+	pushd "${TMP}/svn-packages-copy/pkg-debug-b/trunk/" >/dev/null
+	sed "s/pkgrel=1/pkgrel=2/g;s/pkgname=('pkg-debug-b1' 'pkg-debug-b2')/pkgname='pkg-debug-b2'/g" -i PKGBUILD
+	arch_svn commit -q -m"remove pkg-debug-b1; pkgrel=2" >/dev/null
+	popd >/dev/null
+
+	for arch in ${arches[@]}; do
+		releasePackage testing pkg-debug-a ${arch}
+	done
+	releasePackage testing pkg-debug-b any pkg-debug-b2
+
+	"${curdir}"/../../db-update
+
+	"${curdir}"/../../testing2x pkg-debug-a pkg-debug-b2
+
+	for arch in ${arches[@]}; do
+		checkPackage core pkg-debug-a-1-2-${arch}.pkg.tar.xz ${arch}
+		checkPackage core-${DEBUGSUFFIX} pkg-debug-a-${DEBUGSUFFIX}-1-2-${arch}.pkg.tar.xz ${arch}
+		checkRemovedPackage testing pkg-debug-a ${arch}
+		checkRemovedPackage testing-${DEBUGSUFFIX} pkg-debug-a-${DEBUGSUFFIX} ${arch}
+
+		checkRemovedPackage core pkg-debug-b1 ${arch}
+		checkRemovedPackage core-${DEBUGSUFFIX} pkg-debug-b1-${DEBUGSUFFIX} ${arch}
+		checkRemovedPackage testing pkg-debug-b1 ${arch}
+		checkRemovedPackage testing-${DEBUGSUFFIX} pkg-debug-b2-${DEBUGSUFFIX} ${arch}
+	done
+	checkAnyPackage extra pkg-debug-b2-1-2-any.pkg.tar.xz
+	checkRemovedAnyPackage testing pkg-debug-b2
+}
+
 . "${curdir}/../lib/shunit2"
